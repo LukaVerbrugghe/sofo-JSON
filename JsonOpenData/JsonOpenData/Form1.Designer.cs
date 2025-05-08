11f3aa85e205507d73ec533595e8a77c3a775191
Luka Verbrugghe <verbruggheluka0@gmail.com>
2025-05-08 10:41:07 +0200

    Design + JSON nuget

---
+++ JsonOpenData/JsonOpenData/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace JsonOpenData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInlezenData = new System.Windows.Forms.Button();
+            this.lsbScholen = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnInlezenData
+            // 
+            this.btnInlezenData.Location = new System.Drawing.Point(218, 73);
+            this.btnInlezenData.Name = "btnInlezenData";
+            this.btnInlezenData.Size = new System.Drawing.Size(75, 23);
+            this.btnInlezenData.TabIndex = 0;
+            this.btnInlezenData.Text = "Inlezen data";
+            this.btnInlezenData.UseVisualStyleBackColor = true;
+            // 
+            // lsbScholen
+            // 
+            this.lsbScholen.FormattingEnabled = true;
+            this.lsbScholen.Location = new System.Drawing.Point(79, 143);
+            this.lsbScholen.Name = "lsbScholen";
+            this.lsbScholen.Size = new System.Drawing.Size(597, 277);
+            this.lsbScholen.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbScholen);
+            this.Controls.Add(this.btnInlezenData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInlezenData;
+        private System.Windows.Forms.ListBox lsbScholen;
+    }
+}
+
---
+++ JsonOpenData/JsonOpenData/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonOpenData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("V-SYSTEMC")]
+[assembly: AssemblyProduct("JsonOpenData")]
+[assembly: AssemblyCopyright("Copyright © V-SYSTEMC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51ca57b5-55c0-41ea-a2e8-9359ed0fba5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
